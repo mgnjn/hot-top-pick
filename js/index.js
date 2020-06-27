4fed5f7a374428365c85f06ea4a12abec262e7ce
mgnjn <41850019+mgnjn@users.noreply.github.com>
2020-06-27 17:19:57 -0400

    Update index.js

--- js/index.js
+++
@@ -1,7 +0,0 @@
-var Twit = require('twit');
-var config = require('./config');
-var T = new Twit(config);
-
-T.get('search/tweets', { q: 'node.js' }, function(error, tweets, response) {
-    console.log(tweets);
-});
