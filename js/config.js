0d2649619d41287abe29a1ba9883cc3ea8ed511d
mgnjn <41850019+mgnjn@users.noreply.github.com>
2020-06-27 17:22:36 -0400

    Delete config.js

--- js/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    consumer_key = "FXqrJiySPWIrV0iBGuwI7qXk1";
-    consumer_key_secret = "7H3ev79PgwkBGj7gMu2tsgRv4khkKesAWHZayy1Mbj5u2AdDxL";
-
-    access_token_key = "1273982516295356416-1MimvpgLl2d5Il681ToFonf01tI6SZ";
-    access_token_secret = "KR74eYFn7yAL00PWHwLDULrAPf1hN8qKsIbT7Qmxs3Wrr";
-}
